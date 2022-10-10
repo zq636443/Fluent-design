c6a21f47d7a7df40e2975156c930ebcc415f57f9
zq636443 <109610514+zq636443@users.noreply.github.com>
2022-10-11 00:28:28 +0800

    Update .umirc.ts

--- .umirc.ts
+++ .umirc.ts
@@ -33,7 +33,11 @@ export default defineConfig({
       children: [
         {
           title: 'CSDN',
-          path: 'https://blog.csdn.net/zq2232185805?spm=1000.2115.3001.5343',
+          path: 'https://blog.csdn.net/CarrreyYan_979292?type=blog',
+        },
+        {
+          title:'掘金',
+          path:'https://juejin.cn/user/1785262616087192'
         }
       ],
     },
